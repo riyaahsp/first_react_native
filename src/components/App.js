83db4f2c53d81b18f772dd211a578151b9303a3d
Riyaa Gupta <griyaa03@gmail.com>
2025-02-15 19:55:38 -0700

    added bg

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {View, Text, StyleSheet} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Target Sum..</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    flex: 1,
+  }
+})
+
+export default App;
